58728099fa6d566fcb1e4c09c3f625d87ae7c0df
Alex <alex@alexandrs-macbook-pro.local>
2018-09-29 08:02:05 +0300

    Initial Commit

---
+++ DiceeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceeApp
+//
+//  Created by Alexandr Moshisnky on 29/09/2018.
+//  Copyright © 2018 Alexandr Moshisnky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
